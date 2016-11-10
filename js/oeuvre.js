6c67cc750f6d81c41f6ea544e6eefffb8b33d2f9
Elmar F.Tavares <elmar.atavares@gmail.com>
2016-11-10 12:06:35 +0100

    Niveau 1
    Niveau 1 commencé: oeuvre = bleu de YK
    Soucis de drag & drop a cet état

---
+++ js/oeuvre.js
@@ -0,0 +1,6 @@
+var c=document.getElementById("myCanvas");
+	var ctx=c.getContext("2d");
+	c.width=window.innerWidth/*/2*/;
+	c.height=window.innerHeight;
+	ctx.fillStyle="#002FA7";
+	ctx.fillRect(0,0,c.width,c.height);
