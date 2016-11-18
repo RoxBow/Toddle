fe22768ae746a855a285c3ab06ae4c4cfb2eb51c
Elmar F.Tavares <elmar.atavares@gmail.com>
2016-11-18 10:11:41 +0100

    [EVO] Test Drang n°1
    Premier test du drag sur tablette en assimilant les événements touchs
    aux événements clic de la souris qui eux réagissent au drag & drop.

--- js/oeuvre.js
+++ js/oeuvre.js
@@ -1,4 +1,58 @@
+<<<<<<< HEAD
+var canvas=document.getElementById("canvas");
+	var ctx=canvas.getContext("2d");
+	canvas.width=window.innerWidth/2;
+	canvas.height=window.innerHeight;
+	ctx.fillStyle="#002FA7";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+
+
+
+	 var vert = document.querySelector('#vert');
+  	var rouge = document.querySelector('#rouge');
+  	var jaune = document.querySelector('#jaune');
+  
+	vert.addEventListener('dragstart', function(e) {
+    	e.dataTransfer.setData('text/plain', "vert");
+	});
+	rouge.addEventListener('dragstart', function(e) {
+    	e.dataTransfer.setData('text/plain', "rouge");
+	});
+	jaune.addEventListener('dragstart', function(e) {
+    	e.dataTransfer.setData('text/plain', "jaune");
+	});
+
+	canvas.addEventListener('dragover', function(e) {
+    	e.preventDefault(); // Annule l'interdiction de drop
+    	//c.style.backgroundColor = '#ff9100';
+	});
+
+	canvas.addEventListener('drop', function(e) {
+    	e.preventDefault(); // Annule l'interdiction de drop
+	    //alert(e.dataTransfer.getData('text/plain')); // Affiche le contenu du type MIME « text/plain »
+
+        //Nous peret de tester quelle truc a été dropé avec le setData text/plain
+	  if (e.dataTransfer.getData('text/plain')=="vert") {
+	  	ctx.clearRect(0,0,canvas.width,canvas.height);
+	    ctx.fillStyle="#CBE6A3";
+	    ctx.fillRect(0,0,canvas.width,canvas.height);
+	  } else if (e.dataTransfer.getData('text/plain')=="rouge") {
+	  		ctx.clearRect(0,0,canvas.width,canvas.height);
+	  		ctx.fillStyle="#AA1123";
+	    	ctx.fillRect(0,0,canvas.width,canvas.height);
+      	} else if (e.dataTransfer.getData('text/plain')=="jaune") {
+      			ctx.clearRect(0,0,canvas.width,canvas.height);
+      			ctx.fillStyle="#F8CB00";
+	    		ctx.fillRect(0,0,canvas.width,canvas.height);
+      		}
+	});
+
+	/*document.addEventListener('dragend', function() {
+	    alert("Un Drag & Drop vient de se terminer mais l'événement dragend ne sait pas si c'est un succès ou non.");
+	});*/
+=======
 canvas.width=window.innerWidth/2;
 canvas.height=window.innerHeight;
 ctx.fillStyle="#002FA7";
 ctx.fillRect(0,0,canvas.width,canvas.height);
+>>>>>>> 2171ed3fad9766e584bc5ff3ab1479ea2bb63dbf
--- oeuvre.php
+++ oeuvre.php
@@ -24,7 +24,7 @@ if( !isset($_SESSION['pseudo']) ){
             <p class="name"><?php echo $_SESSION['pseudo']; ?></p>
         </header>
         <section class="leftBloc">
-            <canvas id="canvas"></canvas>
+            <canvas id="canvas" class="box drag-target"></canvas>
         </section>
         <section class="rightBloc">
             <div class="codeBloc">
@@ -34,6 +34,7 @@ if( !isset($_SESSION['pseudo']) ){
                 </div>
                 <button>Valider mon oeuvre <i class="fa fa-play-circle fa-3x" aria-hidden="true"></i></button>
             </div>
+
             <div class="etiqBloc">
                 <h2>Étiquettes</h2>
                 <div class="etiq" id="vert" draggable="true">Vert()</div>
@@ -44,8 +45,94 @@ if( !isset($_SESSION['pseudo']) ){
     </div>
     
     <script src="js/jquery-3.1.1.min.js"></script>
+<<<<<<< HEAD
+    <script src="js/hammer-time.min.js"></script>
+    <script srx="code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <!--<script src="js/main.js"></script>-->
+    <script src="js/global.js"></script>
+    <script src="js/oeuvre.js"></script>
+    <script>
+        /*var element = document.getElementById('vert');
+        Hammer(element).on("tap", function(event) {
+            alert('hello!');
+        });
+        Hammer(element).on("doubletap", function(event) {
+            alert('ntm!');
+        });
+        Hammer($('.etiq').get(0)).on('dragstart', function(event) {
+            console.log('dragstart', event);
+            alert("tamere");
+        });
+
+        Hammer($('.etiq').get(0)).on('drag', function(event){
+          // console.log('drag', event.gesture.deltaX, event.gesture.deltaY)
+          var target = event.target;
+          $(target).css({
+            'transform': 'translate(' + event.gesture.deltaX + 'px,' + event.gesture.deltaY + 'px)'
+          });
+        });
+
+        Hammer(document.body).on('release', function(event){
+          console.log('release', event);
+          event.gesture.preventDefault()
+        });
+
+        Hammer(document.body).on('dragend', function(event) {
+          console.log('dragend', event);
+          
+          $(event.target).css({'transform': 'translate(0,0)'});
+          debugger;
+          var dropEl = document.elementFromPoint(event.gesture.center.pageX, event.gesture.center.pageY);
+          console.log('dropped on', dropEl);
+          if ($(dropEl).hasClass('drop-target')) {
+            console.log('dropped on drop target');
+          }
+        })
+        $(document.body).on('mousedown', '[draggable]', function(event){
+
+          console.log('mousedown', event);
+        })
+        $(document.body).on('mouseup', '[draggable]', function(event){
+          
+          console.log('mouseup', event);
+          event.preventDefault()
+        })*/
+      function touchHandler(event)
+      {
+          var touches = event.changedTouches,
+              first = touches[0],
+              type = "";
+           
+          switch(event.type)
+          {
+              case "touchstart": type = "mousedown";break;
+              case "touchmove":  type="mousemove"; break;       
+              case "touchend":   type="mouseup"; break;
+              default: return;
+          }
+          var simulatedEvent = document.createEvent("MouseEvent");
+          simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                                    first.screenX, first.screenY,
+                                    first.clientX, first.clientY, false,
+                                    false, false, false, 0/*left*/, null);
+           
+          first.target.dispatchEvent(simulatedEvent);
+          event.preventDefault();
+      }
+       
+      function initTouch()
+      {
+         document.addEventListener("touchstart", touchHandler, true);
+         document.addEventListener("touchmove", touchHandler, true);
+         document.addEventListener("touchend", touchHandler, true);
+         document.addEventListener("touchcancel", touchHandler, true);   
+      }
+      $(document).ready(function (){initTouch();});
+    </script>
+=======
     <script src="js/global.js"></script>
     <script src="js/oeuvre.js"></script>
     <script src="js/hammer.min.js"></script>
+>>>>>>> 2171ed3fad9766e584bc5ff3ab1479ea2bb63dbf
 </body>
 </html>
