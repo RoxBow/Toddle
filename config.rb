ccc44ba0e468c7df895a478aea96c3acf6759f58
Elmar F.Tavares <elmar.atavares@gmail.com>
2016-11-21 10:30:30 +0100

    [EVO] Fonctionnalité du niveau 1 : Done
    Le drag & drop fonctionne et opère le bon changement sur l'oeuvre de
    Yves Klein (en vert, rouge et jaune).

--- config.rb
+++ config.rb
@@ -7,6 +7,8 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+asset_cache_buster = :none
+cache = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
--- oeuvre.php
+++ oeuvre.php
@@ -37,102 +37,97 @@ if( !isset($_SESSION['pseudo']) ){
 
             <div class="etiqBloc">
                 <h2>Étiquettes</h2>
-                <div class="etiq" id="vert" draggable="true">Vert()</div>
-                <div class="etiq" id="rouge" draggable="true">Rouge()</div>
-                <div class="etiq" id="jaune" draggable="true">Jaune()</div>
+                <div class="etiq" id="vert" style="position: absolute;">Vert()</div>
+                <div class="etiq" id="rouge">Rouge()</div>
+                <div class="etiq" id="jaune">Jaune()</div>
             </div>
         </section>
     </div>
     
     <script src="js/jquery-3.1.1.min.js"></script>
-<<<<<<< HEAD
     <script src="js/hammer-time.min.js"></script>
-    <script srx="code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-    <!--<script src="js/main.js"></script>-->
+<!--     <script src="code.jquery.com/ui/1.10.4/jquery-ui.js"></script>-->    
+        <!--<script src="js/main.js"></script>-->
     <script src="js/global.js"></script>
     <script src="js/oeuvre.js"></script>
     <script>
-        /*var element = document.getElementById('vert');
-        Hammer(element).on("tap", function(event) {
-            alert('hello!');
-        });
-        Hammer(element).on("doubletap", function(event) {
-            alert('ntm!');
-        });
-        Hammer($('.etiq').get(0)).on('dragstart', function(event) {
-            console.log('dragstart', event);
-            alert("tamere");
-        });
+      $( document ).ready(function() {
+        var element = vert;
+        var element2 = rouge;
+        var element3 = jaune;
 
-        Hammer($('.etiq').get(0)).on('drag', function(event){
-          // console.log('drag', event.gesture.deltaX, event.gesture.deltaY)
-          var target = event.target;
-          $(target).css({
-            'transform': 'translate(' + event.gesture.deltaX + 'px,' + event.gesture.deltaY + 'px)'
-          });
-        });
+          /*Drag & drop du bouton vert*/
 
-        Hammer(document.body).on('release', function(event){
-          console.log('release', event);
-          event.gesture.preventDefault()
-        });
+        var hammertime = new Hammer(element);
+        hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+        var tapX, tapY;
+            hammertime.on('pan', function(e) {
 
-        Hammer(document.body).on('dragend', function(event) {
-          console.log('dragend', event);
-          
-          $(event.target).css({'transform': 'translate(0,0)'});
-          debugger;
-          var dropEl = document.elementFromPoint(event.gesture.center.pageX, event.gesture.center.pageY);
-          console.log('dropped on', dropEl);
-          if ($(dropEl).hasClass('drop-target')) {
-            console.log('dropped on drop target');
-          }
-        })
-        $(document.body).on('mousedown', '[draggable]', function(event){
+                tapX = e.center.x;
+                tapY = e.center.y;
 
-          console.log('mousedown', event);
-        })
-        $(document.body).on('mouseup', '[draggable]', function(event){
-          
-          console.log('mouseup', event);
-          event.preventDefault()
-        })*/
-      function touchHandler(event)
-      {
-          var touches = event.changedTouches,
-              first = touches[0],
-              type = "";
-           
-          switch(event.type)
-          {
-              case "touchstart": type = "mousedown";break;
-              case "touchmove":  type="mousemove"; break;       
-              case "touchend":   type="mouseup"; break;
-              default: return;
-          }
-          var simulatedEvent = document.createEvent("MouseEvent");
-          simulatedEvent.initMouseEvent(type, true, true, window, 1,
-                                    first.screenX, first.screenY,
-                                    first.clientX, first.clientY, false,
-                                    false, false, false, 0/*left*/, null);
-           
-          first.target.dispatchEvent(simulatedEvent);
-          event.preventDefault();
-      }
-       
-      function initTouch()
-      {
-         document.addEventListener("touchstart", touchHandler, true);
-         document.addEventListener("touchmove", touchHandler, true);
-         document.addEventListener("touchend", touchHandler, true);
-         document.addEventListener("touchcancel", touchHandler, true);   
-      }
-      $(document).ready(function (){initTouch();});
+                element.style.left = (tapX-530) + "px";
+                element.style.top = (tapY-180) + "px";
+            });
+            hammertime.on('panend', function(e) {
+                if (tapX>0 && tapX<canvas.width) {
+                  if (tapY>0 && tapY<canvas.height) {
+                      ctx.clearRect(0,0,canvas.width,canvas.height);
+                      ctx.fillStyle="#CBE6A3";
+                      ctx.fillRect(0,0,canvas.width,canvas.height);
+                      element.style.display = "none";
+                  }
+                }
+            });
+
+            /*Drag & drop du bouton rouge*/
+
+        var hammertime2 = new Hammer(element2);
+        hammertime2.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+            hammertime2.on('pan', function(e) {
+
+                tapX = e.center.x;
+                tapY = e.center.y;
+
+                element2.style.left = (tapX-680) + "px";
+                element2.style.top = (tapY-170) + "px";
+            });
+            hammertime2.on('panend', function(e) {
+                if (tapX>0 && tapX<canvas.width) {
+                  if (tapY>0 && tapY<canvas.height) {
+                      ctx.clearRect(0,0,canvas.width,canvas.height);
+                      ctx.fillStyle="#AA1123";
+                      ctx.fillRect(0,0,canvas.width,canvas.height);
+                      element2.style.display = "none";
+                  }
+                }
+            });
+
+            /*Drag & drop du bouton jaune*/
+
+        var hammertime3 = new Hammer(element3);
+        hammertime3.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+            hammertime3.on('pan', function(e) {
+
+                tapX = e.center.x;
+                tapY = e.center.y;
+
+                element3.style.left = (tapX-850) + "px";
+                element3.style.top = (tapY-170) + "px";
+            });
+            hammertime3.on('panend', function(e) {
+                if (tapX>0 && tapX<canvas.width) {
+                  if (tapY>0 && tapY<canvas.height) {
+                      ctx.clearRect(0,0,canvas.width,canvas.height);
+                      ctx.fillStyle="#F8CB00";
+                      ctx.fillRect(0,0,canvas.width,canvas.height);
+                      element3.style.display = "none";
+                  }
+                }
+            });
+      });
+
+
     </script>
-=======
-    <script src="js/global.js"></script>
-    <script src="js/oeuvre.js"></script>
-    <script src="js/hammer.min.js"></script>
->>>>>>> 2171ed3fad9766e584bc5ff3ab1479ea2bb63dbf
 </body>
 </html>
