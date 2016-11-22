6b4e43e5c1e51b858b4dcc2ec0689bf6f2d6a81e
Elmar F.Tavares <elmar.atavares@gmail.com>
2016-11-22 22:58:27 +0100

    [EVO] Avancement niveau 1 du jeu
    Lorsque l'utilisateur ne drop pas dans la zone, le bouton revient à sa
    place.
    Soucis avec les coordonnées pour un drag fluide.

--- js/oeuvre.js
+++ js/oeuvre.js
@@ -1,7 +1,7 @@
 
 var canvas=document.getElementById("canvas");
-	var ctx=canvas.getContext("2d");
-	canvas.width=window.innerWidth/2;
-	canvas.height=window.innerHeight;
-	ctx.fillStyle="#002FA7";
-	ctx.fillRect(0,0,canvas.width,canvas.height);
+var ctx=canvas.getContext("2d");
+canvas.width=window.innerWidth/2;
+canvas.height=window.innerHeight;
+ctx.fillStyle="#002FA7";
+ctx.fillRect(0,0,canvas.width,canvas.height);
--- oeuvre.php
+++ oeuvre.php
@@ -23,25 +23,31 @@ if( !isset($_SESSION['pseudo']) ){
             <img src="img/toddle_text.png" alt="toddle" class="toddle_text">
             <p class="name"><?php echo $_SESSION['pseudo']; ?></p>
         </header>
-        <section class="leftBloc">
-            <canvas id="canvas" class="box drag-target"></canvas>
-        </section>
-        <section class="rightBloc">
-            <div class="codeBloc">
-                <h2>Code</h2>
-                <div>
-                    <p>function myFunction(p1, p2) {<br>return p1 * p2; <br>}</p>
-                </div>
-                <button>Valider mon oeuvre <i class="fa fa-play-circle fa-3x" aria-hidden="true"></i></button>
-            </div>
-
-            <div class="etiqBloc">
-                <h2>Étiquettes</h2>
-                <div class="etiq" id="vert" style="position: absolute;">Vert()</div>
-                <div class="etiq" id="rouge">Rouge()</div>
-                <div class="etiq" id="jaune">Jaune()</div>
-            </div>
-        </section>
+        <div id="jeu">
+          <section class="leftBloc">
+              <canvas id="canvas" class="box drag-target"></canvas>
+          </section>
+          <section class="rightBloc">
+              <div class="codeBloc">
+                  <h2>Code</h2>
+                  <div>
+                      <p>function myFunction(p1, p2) {<br>return p1 * p2; <br>}</p>
+                  </div>
+                  <button>Valider mon oeuvre <i class="fa fa-play-circle fa-3x" aria-hidden="true"></i></button>
+              </div>
+              <div class="etiqBloc" id="etiqBloc">
+                  <h2>Étiquettes</h2>
+                  <div class="etiq" id="vert">ChangerEnVert()</div>
+                  <div class="etiq" id="rouge">ChangerEnRouge()</div>
+                  <div class="etiq" id="jaune">ChangerEnJaune()</div>
+                  <div class="etiq" id="carre">MettreUnCarré()</div>
+                  <div class="etiq" id="violet">ChangerEnViolet()</div>
+                  <div class="etiq" id="rond">MettreUnCercle()</div>
+<!--                   <div class="etiq" id="cercle">Cercle()</div>
+                  <div class="etiq" id="violet">Violet()</div> -->
+              </div>
+          </section>
+        </div>
     </div>
     
     <script src="js/jquery-3.1.1.min.js"></script>
@@ -52,31 +58,36 @@ if( !isset($_SESSION['pseudo']) ){
     <script src="js/oeuvre.js"></script>
     <script>
       $( document ).ready(function() {
+        var jeu = document.getElementById("jeu");
+        var etiqBloc = document.getElementById("etiqBloc");
         var element = vert;
         var element2 = rouge;
         var element3 = jaune;
-
+        var element4 = carre;
+        var element5 = violet;
+        var element6 = rond;
+          /*POUR LE DRAG, IDEE DE LA DIFFERENCE DE DEPLACEMENT, ECRIRE SA SUR UN PAPIER ET UN CRAYON DEMAIN !*/
           /*Drag & drop du bouton vert*/
 
         var hammertime = new Hammer(element);
         hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
         var tapX, tapY;
             hammertime.on('pan', function(e) {
-
                 tapX = e.center.x;
                 tapY = e.center.y;
 
-                element.style.left = (tapX-530) + "px";
-                element.style.top = (tapY-180) + "px";
+                element.style.left = (tapX-100) + "px";
+                element.style.top = (tapY-50) + "px";
             });
             hammertime.on('panend', function(e) {
-                if (tapX>0 && tapX<canvas.width) {
-                  if (tapY>0 && tapY<canvas.height) {
-                      ctx.clearRect(0,0,canvas.width,canvas.height);
-                      ctx.fillStyle="#CBE6A3";
-                      ctx.fillRect(0,0,canvas.width,canvas.height);
-                      element.style.display = "none";
-                  }
+                if ((tapX>0 && tapX<canvas.width)&&(tapY>0 && tapY<canvas.height)) {
+                  ctx.clearRect(0,0,canvas.width,canvas.height);
+                  ctx.fillStyle="#CBE6A3";
+                  ctx.fillRect(0,0,canvas.width,canvas.height);
+                  element.style.display = "none";
+                } else{
+                    element.style.left = "51%";
+                    element.style.top = "59%";
                 }
             });
 
@@ -88,18 +99,19 @@ if( !isset($_SESSION['pseudo']) ){
 
                 tapX = e.center.x;
                 tapY = e.center.y;
-
-                element2.style.left = (tapX-680) + "px";
-                element2.style.top = (tapY-170) + "px";
+                console.log(tapX+"---"+tapY);
+                element2.style.left = (tapX-350) + "px";
+                element2.style.top = (tapY-50) + "px";
             });
             hammertime2.on('panend', function(e) {
-                if (tapX>0 && tapX<canvas.width) {
-                  if (tapY>0 && tapY<canvas.height) {
-                      ctx.clearRect(0,0,canvas.width,canvas.height);
-                      ctx.fillStyle="#AA1123";
-                      ctx.fillRect(0,0,canvas.width,canvas.height);
-                      element2.style.display = "none";
-                  }
+                if ((tapX>0 && tapX<canvas.width)&&(tapY>0 && tapY<canvas.height)) {
+                  ctx.clearRect(0,0,canvas.width,canvas.height);
+                  ctx.fillStyle="#AA1123";
+                  ctx.fillRect(0,0,canvas.width,canvas.height);
+                  element2.style.display = "none";
+                } else{
+                    element2.style.left = "51%";
+                    element2.style.top = "59%";
                 }
             });
 
@@ -112,17 +124,18 @@ if( !isset($_SESSION['pseudo']) ){
                 tapX = e.center.x;
                 tapY = e.center.y;
 
-                element3.style.left = (tapX-850) + "px";
-                element3.style.top = (tapY-170) + "px";
+                element3.style.left = (tapX-100) + "px";
+                element3.style.top = (tapY-100) + "px";
             });
             hammertime3.on('panend', function(e) {
-                if (tapX>0 && tapX<canvas.width) {
-                  if (tapY>0 && tapY<canvas.height) {
-                      ctx.clearRect(0,0,canvas.width,canvas.height);
-                      ctx.fillStyle="#F8CB00";
-                      ctx.fillRect(0,0,canvas.width,canvas.height);
-                      element3.style.display = "none";
-                  }
+                if ((tapX>0 && tapX<canvas.width)&&(tapY>0 && tapY<canvas.height)) {
+                  ctx.clearRect(0,0,canvas.width,canvas.height);
+                  ctx.fillStyle="#F8CB00";
+                  ctx.fillRect(0,0,canvas.width,canvas.height);
+                  element3.style.display = "none";
+                } else{
+                    element3.style.left = "51%";
+                    element3.style.top = "59%";
                 }
             });
       });
