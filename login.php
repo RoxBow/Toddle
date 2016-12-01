ba59856a225471f69ff377d63a500fa3f5e0ba94
RoxBow <v.deplais@gmail.com>
2016-12-01 16:36:09 +0100

    [EVO] MàJ

--- login.php
+++ login.php
@@ -13,12 +13,7 @@ try {
   die();
 }
 
-if(isset($_POST['pseudo'])){
-    $_SESSION['pseudo'] = $_POST['pseudo'];
-    $request = $bdd->prepare("INSERT INTO user (pseudo) VALUES ('".$_SESSION['pseudo']."')"); // prepare command 
-    $request->execute(); // Add pseudo to DB
-}
-else if(isset($_POST['mail'])){
+if(isset($_POST['mail'])){
     $requestMail = $bdd->prepare("UPDATE user SET mail='".$_POST['mail']."' WHERE pseudo='".$_SESSION['pseudo']."'");
     $requestMail->execute();
 }
