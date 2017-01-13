eecf58435fd71e7db7d86ad78b8a06ff4c979980
Elmar F.Tavares <elmar.atavares@gmail.com>
2017-01-13 19:12:48 +0100

    [AJOUT] Niveau 2
    Ajout du niveau avec l'oeuvre mas o menos de frank stella

---
+++ js/oeuvre2.js
@@ -0,0 +1,177 @@
+var canvas, context; 
+var nbLignes = 1;
+var resultat = 27;
+
+/* #####    CHRONO      ##### */
+
+// Update time script
+sec = localSec;
+min = localMin;
+
+// When page is left
+$(window).bind('beforeunload',function(){
+    localStorage.setItem("seconde", $("#sec").val() );
+    localStorage.setItem("minute", $("#min").val() );
+});
+
+$(document).ready(function() {
+    /* ### CHRONO ### */
+    $("#sec").val(localSec);
+    $("#min").val(localMin);
+    chrono();
+});
+
+function init() {
+    canvas = document.getElementById("mycanvas");
+    context = canvas.getContext("2d");
+    canvas.height=450;
+    canvas.width=700;
+    $("#nbLignes").text(nbLignes);
+    render();
+}
+
+//nombre de lignes
+$("#valider").on("click",function(){
+    var lines=$("#nbLignes").text();
+    if (lines==resultat){
+        win();
+    } else {
+        lose();
+    }
+});
+
+$("#plus").on("click",function(){
+    if (nbLignes>-1 && nbLignes <31 && nbLignes!=31) {
+        nbLignes += 1;
+        if (nbLignes==31) {
+            nbLignes=30;
+        }
+        $("#nbLignes").text(nbLignes);
+        render();
+    }
+
+});
+$("#moins").on("click",function(){
+    if (nbLignes>=0 && nbLignes <=30) {
+        nbLignes -= 1;
+        if (nbLignes==-1) {
+            nbLignes=0;
+        }
+        $("#nbLignes").text(nbLignes);
+        render();
+    }
+});
+
+
+
+// Draw forms
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  var vars = 0;
+
+  for (var i = 0; i < nbLignes; i++) {
+
+    //Angle de la pente
+    vars= i*2;
+
+//Ligne gris clair
+    context.beginPath();
+    creationLine(10 * i, vars);
+    context.closePath();  
+    
+    context.lineWidth = 2;
+    context.strokeStyle = '#C5BDB2';
+    context.stroke();
+
+//Deuxieme ligne
+
+    context.beginPath();
+    //espacement entre les lignes    
+    creationLine2(10*i+2,vars);
+    context.closePath();  
+    
+    context.lineWidth = 2;
+    context.strokeStyle = '#565043';
+    context.stroke();
+
+//3ieme ligne
+    
+    context.beginPath();
+    //espacement entre les lignes    
+    creationLine2(10*i+4,vars);
+    context.closePath();  
+    
+    context.lineWidth = 2;
+    context.strokeStyle = '#565043';
+    context.stroke();
+
+//4ieme ligne
+    
+    context.beginPath();
+    //espacement entre les lignes    
+    creationLine2(10*i+6,vars);
+    context.closePath();  
+    
+    context.lineWidth = 2;
+    context.strokeStyle = '#565043';
+    context.stroke();
+
+//5ieme ligne
+    
+    context.beginPath();
+    //espacement entre les lignes    
+    creationLine2(10*i+8,vars);
+    context.closePath();  
+    
+    context.lineWidth = 2;
+    context.strokeStyle = '#565043';
+    context.stroke();
+    
+  }
+}
+
+function creationLine(y,x) {
+    var variations = x;
+    var x;
+    y = y + 75;
+
+    context.moveTo(20, y-20);
+    context.lineTo(130,y - 60);
+    context.moveTo(290-variations,y);
+    context.lineTo(130, y - 60);
+    context.moveTo(290-variations,y);
+    context.lineTo(440-variations,y);
+    context.moveTo(440-variations,y);
+    context.lineTo(540,y + 50); 
+    context.moveTo(540,y + 50);
+    context.lineTo(670,y);
+}
+
+function creationLine2(y,x) {
+    var variations = x;
+    var x;
+    y = y + 75;
+
+    context.moveTo(20, y-20);
+    context.lineTo(130,y - 60);
+    context.moveTo(290-variations,y);
+    context.lineTo(130, y - 60);
+    context.moveTo(290-variations,y);
+    context.lineTo(440-variations,y);
+    context.moveTo(440-variations,y);
+    context.lineTo(540,y + 50); 
+    context.moveTo(540,y + 50);
+    context.lineTo(670,y);
+}
+
+function win(){
+    alert('Bravo, tu as réussi !');
+}
+function lose(){
+    alert("Regarde atentivement l'oeuvre !");
+}
+// When document is loaded
+(function () {
+    init();
+})();
+
---
+++ oeuvre2.php
@@ -0,0 +1,79 @@
+<?php 
+
+session_start();
+
+if( !isset($_SESSION['pseudo']) ){
+    header("location: name.php");
+}
+
+/*
+### Chrono mode PHP ###
+$start = new DateTime($_SESSION['start']); // put old date in date object
+
+$_SESSION['end'] = date("h:i:s"); // get actual date
+$end = new DateTime($_SESSION['end']); // put actual date in date object
+
+$interval = $start->diff($end); // put diff date in interval
+print $interval->format("Tu as mis %H:%I:%S"); // display diff between date
+*/
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Toodle - Interactive game</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/oeuvre2.css" media="all">
+</head>
+<body>
+    <?php
+        include 'credits.php';
+    ?>
+    <div class="container">
+       <header>
+            <img src="img/toddle_form.png" alt="toddle" class="toddle_form">
+            <img src="img/toddle_text.png" alt="toddle" class="toddle_text">
+            <div class="blockRight">
+                <p class="name"><?php echo $_SESSION['pseudo']." - "; ?></p>
+                <form name="chrono" class="chrono">
+                    <input type="text" name="minute" id="min">min
+                    <input type="text" name="seconde" id="sec">s
+                </form><br>
+           </div>
+        </header>
+            <section>
+                <div class="consignes">
+                    <p>A l'aide de la fonction <span class="rose">DessinerLignes()</span>, ajoute ou supprime des lignes afin de recréer l'oeuvre.</p>
+                    <br>
+                </div>
+                <canvas id="mycanvas"></canvas>
+                <div class="button" id="plus"><p>+</p></div>
+                <div class="button" id="moins"><p>-</p></div>
+
+                <div class="result">
+                    <div class="buttonV" id="but">
+                        <img src="img/flag.png" alt="Indice"/>
+                    </div>
+                    <p>DessinerLignes(<span id="nbLignes"></span>)</p>
+                    <div class="buttonV" id="valider">
+                        <i class="fa fa-check fa-2x" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </section>
+        <footer>
+            <img src="img/logo_pompidou.png" alt="pompidou">
+            <img src="img/logo_gris_avec.png" alt="avec">
+        </footer>
+    </div>
+    
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/global.js"></script>
+    <script src="js/chrono.js"></script>
+    <script src="js/oeuvre2.js"></script>
+    <script>
+
+    </script>
+</body>
+</html>
